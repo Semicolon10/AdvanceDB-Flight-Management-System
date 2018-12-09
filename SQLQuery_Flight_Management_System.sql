452e1574046687cf5962998f797f7aaacacc21c9
Sewmal <32815389+Sewmal@users.noreply.github.com>
2018-12-10 00:18:58 +0530

    Updated Database creation file with constraints

--- SQLQuery_Flight_Management_System.sql
+++ SQLQuery_Flight_Management_System.sql
@@ -37,6 +37,12 @@ CREATE TABLE Aircraft(
 	CONSTRAINT pk_AircraftCode PRIMARY KEY(AircraftCode)
 );
 
+ALTER TABLE Aircraft
+ADD CONSTRAINT fk_AirlineCode_Aircraft FOREIGN KEY(AirlineCode) REFERENCES Airline(AirlineCode);
+
+ALTER TABLE Aircraft
+ADD CONSTRAINT fk_FlightNo_Aircraft FOREIGN KEY(FlightNo) REFERENCES Flight(FlightNo);
+
 CREATE TABLE Pilot(
 	StaffID VARCHAR(20) NOT NULL,
 	FirstName VARCHAR(50) NOT NULL,
@@ -54,6 +60,12 @@ CREATE TABLE Pilot(
 	CONSTRAINT pk_StaffID PRIMARY KEY(StaffID)
 );
 
+ALTER TABLE Pilot
+ADD CONSTRAINT fk_StaffID_Pilot FOREIGN KEY(StaffID) REFERENCES Aircrew(StaffID);
+
+ALTER TABLE Pilot
+ADD CONSTRAINT fk_PassportNo_Pilot FOREIGN KEY(PassportNo) REFERENCES Aircrew(PassportNo);/*There is an error*/
+
 CREATE TABLE FlightAttendant(
 	StaffID VARCHAR(20) NOT NULL,
 	FirstName VARCHAR(50) NOT NULL,
@@ -68,6 +80,12 @@ CREATE TABLE FlightAttendant(
 	TotalFlyingHours TIME NOT NULL,
 	CONSTRAINT pk_FlightAttendant_StaffID PRIMARY KEY(StaffID)
 );
+
+ALTER TABLE FlightAttendant
+ADD CONSTRAINT fk_PassportNo_FlightAttendant FOREIGN KEY(PassportNo) REFERENCES Aircrew(PassportNo);/*There is an error*/
+
+ALTER TABLE FlightAttendant
+ADD CONSTRAINT fk_StaffID_FlightAttendant FOREIGN KEY(StaffID) REFERENCES Aircrew(StaffID);
 
 CREATE TABLE Passenger(
 	PassportNo VARCHAR(30) NOT NULL,
@@ -92,10 +110,14 @@ CREATE TABLE Minor(
 	PassportNo VARCHAR(30) NOT NULL,
 	AccompanyPassportNumber VARCHAR(30) NOT NULL,
 	CONSTRAINT pk_PassportNo_Minor PRIMARY KEY(PassportNo)
-	/*CONSTRAINT fk_PassportNo FOREIGN KEY(PassportNo) REFERENCES Passenger(PassportNo),
-	CONSTRAINT fk_AccompanyPassportNumber FOREIGN KEY(AccompanyPassportNumber) REFERENCES Passenger(PassportNo)*/
-
 );
+
+ALTER TABLE Minor
+ADD CONSTRAINT fk_PassportNo_Minor FOREIGN KEY(PassportNo) REFERENCES Passenger(PassportNo);
+
+ALTER TABLE Minor
+ADD CONSTRAINT fk_AccompanyPassportNumber_Minor FOREIGN KEY(AccompanyPassportNumber) REFERENCES Passenger(PassportNo);
+
 
 CREATE TABLE Arrival(
 	LegNo VARCHAR(20) NOT NULL,
@@ -104,6 +126,9 @@ CREATE TABLE Arrival(
 	BaggageBeltNo VARCHAR(20) NOT NULL,
 	CONSTRAINT pk_LegNo_Arrival PRIMARY KEY(LegNo)
 );
+
+ALTER TABLE Arrival
+ADD CONSTRAINT fk_LegNo_Arrival FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
 
 CREATE TABLE Departure(
 	LegNo VARCHAR(20) NOT NULL,
@@ -114,17 +139,26 @@ CREATE TABLE Departure(
 	CONSTRAINT pk_LegNo_Departure PRIMARY KEY(LegNo)
 );
 
+ALTER TABLE Departure
+ADD CONSTRAINT fk_LegNo_Departure FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
+
 CREATE TABLE Delayed(
 	LegNo VARCHAR(20) NOT NULL,
 	Reason VARCHAR(500) NOT NULL,
 	CONSTRAINT pk_LegNo_Delayed PRIMARY KEY(LegNo)
 );
 
+ALTER TABLE Delayed
+ADD CONSTRAINT fk_LegNo_Delayed FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
+
 CREATE TABLE Canceled(
 	LegNo VARCHAR(20) NOT NULL,
 	Reason VARCHAR(500) NOT NULL,
 	CONSTRAINT pk_LegNo_Canceled PRIMARY KEY(LegNo)
 );
+
+ALTER TABLE Canceled
+ADD CONSTRAINT fk_LegNo_Canceled FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
 
 CREATE TABLE Provide_Services_Airline(
 	AirlineCode VARCHAR(20) NOT NULL,
@@ -151,11 +185,14 @@ CREATE TABLE Provide_Services_Airport(
 );
 
 CREATE TABLE Airline_Contact_No(
-	AirlineCode VARCHAR(10) NOT NULL,
+	AirlineCode VARCHAR(20) NOT NULL,
 	ContactNo VARCHAR(20) NOT NULL,
 	/*CONSTRAINT fk_AirlineCode FOREIGN KEY(AirlineCode) REFERENCES Provide_Services_Airline(AirlineCode),*/
 	CONSTRAINT pk_AirlineCode_ContactNo PRIMARY KEY(AirlineCode,ContactNo)
 );
+
+ALTER TABLE Airline_Contact_No
+ADD CONSTRAINT fk_AirlineCode_Airline_Contact_No FOREIGN KEY(AirlineCode) REFERENCES Airline(AirlineCode);
 
 /*CREATE TABLE Aircrew_Contact_No(
 	StaffID VARCHAR(20) NOT NULL,
@@ -168,9 +205,10 @@ CREATE TABLE Passenger_Contact_No(
 	PassportNo VARCHAR(30) NOT NULL,
 	ContactNo VARCHAR(20) NOT NULL,
 	CONSTRAINT pk_PassportNo_ContactNo PRIMARY KEY(PassportNo,ContactNo)
-	/*CONSTRAINT fk_PassportNo FOREIGN KEY (PassportNo) REFERENCES Passenger(PassportNo)*/
 );
 
+ALTER TABLE Passenger_Contact_No
+ADD CONSTRAINT fk_PassportNo_Passenger_Contact_No FOREIGN KEY (PassportNo) REFERENCES Passenger(PassportNo);
 
 CREATE TABLE Aircrew(
 	StaffID VARCHAR(20) NOT NULL, 
@@ -189,6 +227,12 @@ CREATE TABLE Aircrew(
 	CONSTRAINT pk_StaffID_Aircrew PRIMARY KEY(StaffID)
 );
 
+ALTER TABLE Aircrew
+ADD CONSTRAINT fk_AirlineCode_Aircrew FOREIGN KEY(AirlineCode) REFERENCES Airline(AirlineCode);
+
+ALTER TABLE Aircrew
+ADD CONSTRAINT fk_LegNo_Aircrew FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
+
 CREATE TABLE Flight(
 	FlightNo VARCHAR(20) NOT NULL,
 	FlightPath VARCHAR(500) NOT NULL,
@@ -196,6 +240,9 @@ CREATE TABLE Flight(
 	FlightType VARCHAR(100) NOT NULL,
 	CONSTRAINT pk_FlightNo PRIMARY KEY(FlightNo)
 );
+
+ALTER TABLE Flight
+ADD CONSTRAINT fk_AirlineCode_Flight FOREIGN KEY(AirlineCode) REFERENCES Airline(AirlineCode);
 
 CREATE TABLE Pilot_Contact_No(
 	StaffID VARCHAR(20) NOT NULL,
@@ -211,7 +258,6 @@ CREATE TABLE FlightAttendant_Contact_No(
 	CONSTRAINT fk_StaffID_FlightAttendant_Contact_No FOREIGN KEY(StaffID) REFERENCES Aircrew(StaffID)
 );
 
-
 CREATE TABLE Landing(
 	FlightNo VARCHAR(20) NOT NULL,
 	AirportName VARCHAR(30) NOT NULL,
@@ -219,12 +265,18 @@ CREATE TABLE Landing(
 	CONSTRAINT fk_FlightNo_Landing FOREIGN KEY (FlightNo) REFERENCES Flight(FlightNo)
 );
 
+ALTER TABLE Take_Off
+ADD CONSTRAINT fk_AirportName_Landing FOREIGN KEY(AirportName) REFERENCES Airport(AirportName);
+
 CREATE TABLE Take_Off(
 	FlightNo VARCHAR(20) NOT NULL,
 	AirportName VARCHAR(30) NOT NULL,
 	CONSTRAINT pk_FlightNo_AirportName_Take_Off PRIMARY KEY(FlightNo,AirportName),
 	CONSTRAINT fk_FlightNo_Take_Off FOREIGN KEY(FlightNo) REFERENCES Flight(FlightNo)
 );
+
+ALTER TABLE Take_Off
+ADD CONSTRAINT fk_AirportName_Take_Off FOREIGN KEY(AirportName) REFERENCES Airport(AirportName);
 
 CREATE TABLE Shedule_Days(
 	FlightNo VARCHAR(20) NOT NULL,
@@ -235,7 +287,6 @@ CREATE TABLE Shedule_Days(
 	CONSTRAINT fk_FlightNo_Shedule_Days FOREIGN KEY(FlightNo) REFERENCES Flight(FlightNo)
 );
 
-
 CREATE TABLE Flight_Leg(
 	LegNo VARCHAR(20) NOT NULL,
 	DateOfFlight DATE NOT NULL,
@@ -243,9 +294,13 @@ CREATE TABLE Flight_Leg(
 	AircraftCode VARCHAR(20) NOT NULL,
 	FlightNo VARCHAR(20) NOT NULL,
 	CONSTRAINT pk_LegNo PRIMARY KEY(leGNo)
-	/*CONSTRAINT fk_AircraftCode FOREIGN KEY(AircraftCode) REFERENCES Aircraft(AircraftCode)*/
-	/*CONSTRAINT fk_FlightNo FOREIGN KEY(Flight) REFERENCES Flight(FlightNo)*/
 );
+
+ALTER TABLE Flight_Leg
+ADD CONSTRAINT fk_FlightNo_Flight_Leg FOREIGN KEY(FlightNo) REFERENCES Flight(FlightNo);
+
+ALTER TABLE Flight_Leg
+ADD CONSTRAINT fk_AircraftCode_Flight_Leg FOREIGN KEY(AircraftCode) REFERENCES Aircraft(AircraftCode);
 
 CREATE TABLE Reservation(
 	TicketNo VARCHAR(100) NOT NULL,
@@ -257,3 +312,14 @@ CREATE TABLE Reservation(
 	LegNo VARCHAR(20) NOT NULL,
 	CONSTRAINT pk_TicketNo PRIMARY KEY(TicketNo)
 );
+ALTER TABLE Reservation
+ADD CONSTRAINT fk_PassportNo_Reservation FOREIGN KEY(PassportNo) REFERENCES Passenger(PassportNo);
+
+ALTER TABLE Reservation
+ADD CONSTRAINT fk_LegNo_Reservation FOREIGN KEY(LegNo) REFERENCES Flight_Leg(LegNo);
+
+ALTER TABLE Reservation
+ADD CONSTRAINT uq_SeatNo_Reservation UNIQUE(SeatNo);
+
+ALTER TABLE Reservation
+ADD CONSTRAINT uq_PassportNo_Reservation UNIQUE(PassportNo);
